74be60f19b28479d1df2c138f36df56fdb38b0b1
yilkan0510 <95488618+yilkan0510@users.noreply.github.com>
2025-03-16 18:04:45 +0100

    Update Quiz.js
    Using index.css on the quiz now

--- src/frontend/src/components/Quiz.js
+++ src/frontend/src/components/Quiz.js
@@ -14,6 +14,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { unescape } from 'lodash';
 import he from 'he';
+import './index.css';
 
 function Quiz({ onLogout }) {
   // State Management
